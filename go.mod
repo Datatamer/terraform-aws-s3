c06b01dcdbad454ae3a531294165e7d9558143d7
Hugo Barros <hugo.barros@caylent.com>
2021-06-17 09:46:48 -0300

    adding tests

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/caylent/tamr-terraform-aws-s3
+
+go 1.13
+
+require (
+	github.com/gruntwork-io/terratest v0.35.6
+	github.com/stretchr/testify v1.4.0 // indirect
+)
---
+++ tests/unencrypted_test.go
@@ -0,0 +1,55 @@
+package tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformS3CreateUnencryptedBucket(t *testing.T) {
+	t.Parallel()
+
+	expectedName := fmt.Sprintf("terratest-aws-s3-example-%s", strings.ToLower(random.UniqueId()))
+
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/minimal",
+
+		Vars: map[string]interface{}{
+			"test_bucket_name": expectedName,
+			// "tag_bucket_environment": expectedEnvironment,
+			// "with_policy": "true",
+		},
+
+		// Environment variables to set when running Terraform
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	b, err := terraform.OutputMapOfObjectsE(t, terraformOptions, "test-bucket")
+
+	if err != nil {
+		logger.Log(t, err)
+	}
+
+	assert.Equal(t, expectedName, b["bucket_name"].(string))
+	// aws.AssertS3BucketPolicyExists(t, awsRegion, b["bucket_name"].(string))
+
+}
+
+/*
+map[bucket_name:terratest-aws-s3-example-a5qoov ro_policy_arn:arn:aws:iam::131578276461:policy/terratest-aws-s3-example-a5qoov-read-only-J4XMR5 rw_policy_arn:arn:aws:iam::131578276461:policy/terratest-aws-s3-example-a5qoov-read-write-J4XMR5]
+*/
