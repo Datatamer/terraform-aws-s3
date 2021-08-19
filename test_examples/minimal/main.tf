54928a2f9689ec2b135eebf91ada64212df35a27
Hugo Barros <hugo.barros@caylent.com>
2021-08-19 16:10:20 -0300

    adds actions variables into example

--- test_examples/minimal/main.tf
+++ test_examples/minimal/main.tf
@@ -1,7 +1,9 @@
 module "minimal" {
   # source           = "git::https://github.com/Datatamer/terraform-aws-s3?ref=1.0.0"
-  source           = "../../"
-  bucket_name      = var.test_bucket_name
-  read_only_paths  = var.read_only_paths
-  read_write_paths = var.read_write_paths
+  source             = "../../"
+  bucket_name        = var.test_bucket_name
+  read_only_paths    = var.read_only_paths
+  read_write_paths   = var.read_write_paths
+  read_write_actions = var.read_write_actions
+  read_only_actions  = var.read_only_actions
 }
