fb3a0a3b4983ff6b7f5d6ebff51242a1ca3b6da9
Hugo Barros <hugo.barros@caylent.com>
2021-09-15 16:07:04 -0300

    Simplifying comments regarding test_structure + parallelism (in local tests)

--- test/s3_test.go
+++ test/s3_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
@@ -97,54 +96,14 @@ func initTestCases() []BucketTestCase {
 }
 
 func TestTerraformS3Module(t *testing.T) {
-
-	// os.Setenv("SKIP_", "true")
+	// Override random region if needed.
 	// os.Setenv("TERRATEST_REGION", "us-east-1")
 
-	// For convenience - uncomment these as well as the "os" import
-	// when doing local testing if you need to skip any sections.
-	//
-	// The good usage for skipping stages is local testing, when you want to make sure your test code is fine,
-	// and not waste time building new infrastructure every time.
-	// A nice approach for doing that would be:
-	// 1- Make sure there are no local files in the dirs (`rm -rf .test-dir terraform.tfstate*`)
-	// 2- Skip teardown* and run tests. (create and keep infrastructure, tfstate will be into local folders)
-	// 3- In your next run skip `pick*` steps (which makes sure random values in Terratest state won't be updated)
-	//    obs: if you forget and run tests once, you can make use of `terraform.tfstate.backup` file to restore it
-	// 4- Run local Tests; Do your thing;
-	// 5- When you are done and want to destroy the infrastructure -> comment back teardown* steps
-	// 6- Comment back pick* steps.
-
-	// os.Setenv("SKIP_pick_new_randoms", "true")
-	// os.Setenv("SKIP_setup_options", "true")
-	// os.Setenv("SKIP_create_bucket", "true")
-	// os.Setenv("SKIP_validate_bucket", "true")
-	// os.Setenv("SKIP_create_ro_objects", "true")
-	// os.Setenv("SKIP_setup_role_options", "true")
-	// os.Setenv("SKIP_create_role", "true")
-	// os.Setenv("SKIP_validate_bucket_and_policies", "true")
-
-	// Warning: if you skip these steps, Terratest state will be stored in local folder under .test-data inside each TF dir you skip.
-	// These are purposedly stored in the known TF dir (not tmp folder) to make sure you can re-run tests on them at any time.
-	// Remember removing those folders after you finish your tests so that it won't affect the next time you run local tests.
-	// os.Setenv("SKIP_teardown", "true")
-	// os.Setenv("SKIP_teardown_role", "true")
-	//
 	// string to be used as body for files to be created
 	const test_body = "test"
 
 	// list of different buckets that will be created to be tested
 	bucketTestCases := initTestCases()
-
-	// When using test_structure functions + parallel tests with random IDs we will run into consistency problems.
-	// This is an easy/lazy way to deal with it.
-	if test_structure.SkipStageEnvVarSet() && len(bucketTestCases) > 1 {
-		logger.Log(t,
-			"Won't run tests using SKIP_* vars having multiple cases. Temporary folders are disabled when using SKIP_* (local testing). Not having different folders for each testCase will generate conflicts with state files.")
-		t.FailNow()
-		// Another solution would be to simply truncate the list of cases instead of failing.
-		// bucketTestCases = bucketTestCases[:1]
-	}
 
 	for _, testCase := range bucketTestCases {
 		// The following is necessary to make sure testCase's values don't
@@ -155,15 +114,11 @@ func TestTerraformS3Module(t *testing.T) {
 			t.Parallel()
 
 			// These will create a tempTestFolder for each bucketTestCase.
-			// Also, if any of SKIP_* env vars are set, it won't create temp folders in order to store configuration that must be retained
-			// through different local tests when skipping stages.
-			// (e.g. `awsRegion` and `uniqueId`.
 			tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "..", "test_examples/minimal")
 			roleTempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "..", "test_examples/helpers/iam_lpp")
 
 			// this stage will generate a random `awsRegion` and a `uniqueId` to be used in tests.
 			test_structure.RunTestStage(t, "pick_new_randoms", func() {
-				// Pick a random AWS region to test in. This helps ensure your code works in all regions.
 				usRegions := []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}
 				// This function will first check for the Env Var TERRATEST_REGION and return its value if != ""
 				awsRegion := aws.GetRandomStableRegion(t, usRegions, nil)
