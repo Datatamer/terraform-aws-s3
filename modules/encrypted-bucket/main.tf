8b2d8f3bf8fc0d44bd658ee6c5d82ea816d9219f
Federico Vazquez <federico.vazquez@caylent.com>
2022-04-19 16:57:16 -0300

    Adds enable public access block functionality.

--- modules/encrypted-bucket/main.tf
+++ modules/encrypted-bucket/main.tf
@@ -1,7 +1,14 @@
 #tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "new_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   force_destroy = var.force_destroy
   tags          = var.tags
@@ -19,13 +26,18 @@ resource "aws_s3_bucket_policy" "sse_bucket_policy" {
   )
 }
 
-# Sets S3 bucket default bucket encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_for_new_bucket" {
+# Sets S3 bucket ACL
+resource "aws_s3_bucket_acl" "acl_for_new_bucket" {
+  bucket = aws_s3_bucket.new_bucket.id
+  acl    = "private"
+}
+
+# Enabling S3 bucket public access block
+resource "aws_s3_bucket_public_access_block" "for_new_bucket" {
   bucket = aws_s3_bucket.new_bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-    }
-}
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
